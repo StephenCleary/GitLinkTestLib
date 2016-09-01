9714a8b4be7bed31ad0a12e8a04614b0495f9042
StephenCleary <scleary@learningmachine.com>
2016-09-01 13:44:22 -0400

    Athena.

---
+++ GitLinkTestLib/GitLinkTestLib/Test.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitLinkTestLib
+{
+    public static class Test
+    {
+        public static void StepIntoMe()
+        {
+            var dir = Environment.CurrentDirectory;
+            Trace.WriteLine("Hi, I'm at: " + dir);
+        }
+    }
+}
