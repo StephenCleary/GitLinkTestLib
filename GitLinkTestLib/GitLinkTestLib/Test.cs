2da8e5d4d8143fa9d945ccf5773241014fd78588
Stephen Cleary <github.ourteddybear@xoxy.net>
2016-09-01 18:20:39 -0400

    Add asynchronous methods for more test cases.

--- GitLinkTestLib/GitLinkTestLib/Test.cs
+++ GitLinkTestLib/GitLinkTestLib/Test.cs
@@ -9,10 +9,24 @@ namespace GitLinkTestLib
 {
     public static class Test
     {
-        public static void StepIntoMe()
+        public static void SynchronousMethod()
         {
             var dir = Environment.CurrentDirectory;
             Trace.WriteLine("Hi, I'm at: " + dir);
+        }
+
+        public static async Task AsynchronousMethod()
+        {
+            var dir = Environment.CurrentDirectory;
+            Trace.WriteLine("Hi, I'm at: " + dir);
+            await Task.Yield();
+        }
+
+        public static Task TaskReturningSynchronousMethod()
+        {
+            var dir = Environment.CurrentDirectory;
+            Trace.WriteLine("Hi, I'm at: " + dir);
+            return Task.CompletedTask;
         }
     }
 }
